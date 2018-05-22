bd9b3824b58953413cc9177bdfdf89b8fe9d9051
Yuhsak <yuhsak.inoue@gmail.com>
2018-05-22 14:39:47 +0900

    Initial commit

---
+++ spec/atom-livy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomLivyView from '../lib/atom-livy-view';
+
+describe('AtomLivyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
